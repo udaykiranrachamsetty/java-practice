6da28d90522d1822d9886c3516d4308b71b1e4dd
udaykiranrachamsetty <82595127+udaykiranrachamsetty@users.noreply.github.com>
2024-03-02 16:20:25 +0530

    reverse a word

---
+++ src/string/reverseawordinstring.java
@@ -0,0 +1,17 @@
+package string;
+
+public class reverseawordinstring {
+    public static void main(String[] args) {
+        String s = "this is an amazing program";
+        String reverse = reverse(s);
+        System.out.println(reverse);
+    }
+    public static String reverse(String s){
+        String[] arr = s.split(" ");
+        StringBuilder ans = new StringBuilder();
+        for(int i = arr.length-1;i >= 0 ; i--){
+            ans.append(arr[i]).append(" ");
+        }
+        return ans.toString();
+    }
+}
